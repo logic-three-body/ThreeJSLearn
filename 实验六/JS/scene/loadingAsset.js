3b9ea77fd2fdc03a054146181aa065fbacef4caa
logic-three-body <1552451580@qq.com>
2021-05-08 19:26:02 +0800

    更改提示词

---
+++ 实验六/JS/scene/loadingAsset.js
@@ -0,0 +1,9 @@
+var container, raycaster, stats;
+
+var camera, scene, renderer, composer, controls, velocity;
+var blocker, instructions;
+var moveLeft, moveForward, moveBackward, moveRight;
+var loader;
+var audioListener, soundFilter, soundAreaAnalyser, soundOutsideAnalyser;
+var soundArea, collisionArea, lightArea, lightOutside;
+
